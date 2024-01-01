6b4135b8d334200ff579fa5b155e9897fa8b520d
BreadStixFTW <77415101+BreadStixFTW@users.noreply.github.com>
2024-01-01 00:48:22 -0500

    Documented the feeder commands
    Documented the feeder commands.

--- src/main/java/frc/robot/commands/FeederCommands.java
+++ src/main/java/frc/robot/commands/FeederCommands.java
@@ -3,14 +3,38 @@ package frc.robot.commands;
 import frc.robot.subsystems.Feeder;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 
+/**
+ * Contains the methods that return the feeder commands.
+ */
 public final class FeederCommands {
+    /**
+     * Gets the normal fire command for the
+     * feeder subsystem.
+     * 
+     * @return The normal fire command.
+     */
     public static InstantCommand normalFireCommand() {
+        /*
+         * Returns an instant command that runs the normal
+         * fire command.
+         */
         return new InstantCommand(
             () -> Feeder.getInstance().normalFire(),
             Feeder.getInstance()
         );
     }
+
+    /**
+     * Gets the rapid fire command for the
+     * feeder subsystem.
+     * 
+     * @return The rapid fire command.
+     */
     public static InstantCommand rapidFireCommand() {
+        /*
+         * Returns an instant command that runs the rapid
+         * fire command.
+         */
         return new InstantCommand(
             () -> Feeder.getInstance().rapidFire(),
             Feeder.getInstance()
